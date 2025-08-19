09e7ac3644dffe91d7189ea076b4d9a5586f1e61
Brian Rono C. K. <bkcrono@gmail.com>
2025-08-19 14:04:48 +0200

    Initial Repo Structure

---
+++ Part1_RESTful-API-SpringBoot/src/main/java/com/example/springrest/Part1ResTfulApiSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Part1ResTfulApiSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Part1ResTfulApiSpringBootApplication.class, args);
+    }
+
+}
