6ebdbd76df143dc7e4de266aacc5a4a71ed8c839
Brian Rono C. K. <bkcrono@gmail.com>
2025-09-03 11:06:31 +0200

    Part 2 - Chapter 4: Custom Hooks(useDebounced.js)

---
+++ Part2_Responsive-Frontend-React/src/hooks/useDebounced.js
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react';
+
+export default function useDebounced(value, delay = 300) {
+  const [debouncedValue, setDebouncedValue] = useState(value);
+
+  useEffect(() => {
+    const handler = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    // Cancel the timeout if value or delay changes, or on unmount
+    return () => {
+      clearTimeout(handler);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
